5e8c8a04b44233efd5291d7fad07c1b1758b269d
aburzyns <aburzyns@gmail.com>
2025-04-11 11:10:26 +0200

    Add files via upload

--- citybuilder.cpp
+++ citybuilder.cpp
@@ -6,23 +6,23 @@
 
 using namespace std;
 
-void gland(int tab[20][20])
+void gland(int tab[50][50])
 {
     srand(time(NULL));
 
-    for(int i = 0; i < 20; i++)
+    for(int i = 0; i < 50; i++)
     {
-        for(int j = 0; j < 20; j++)
+        for(int j = 0; j < 50; j++)
         {
             tab[i][j] = 0;
         }
     }
 
-    for(int o = 0; o < 3; o++)
+    for(int o = 0; o < 10; o++)
     {
         int v1, v2;
-        v1 = rand() % 20;
-        v2 = rand() % 20;
+        v1 = rand() % 50;
+        v2 = rand() % 50;
 
         for(int m = -2; m < 3; m++)
         {
@@ -85,7 +85,7 @@ void gland(int tab[20][20])
                 if(tab[v1 + m][v2 + n] == 0)
                 {
                     int random = rand() % 10;
-                    if(random == 0 || random == 1 || random == 2 || random == 3 || random == 4 || random == 5 || random == 6 || random == 7)
+                    if(random == 0 || random == 1 || random == 2 || random == 3 || random == 4 || random == 5 || random == 6)
                     {
                         tab[v1 + m][v2 + n] = 0;   
                     }
@@ -100,26 +100,67 @@ void gland(int tab[20][20])
                 }
             }
         }
+
+        for(int m = -5; m < 6; m++)
+        {
+            for(int n = -5; n < 6; n++)
+            {
+                if(tab[v1 + m][v2 + n] == 0)
+                {
+                    int random = rand() % 10;
+                    if(random == 0 || random == 1 || random == 2 || random == 3 || random == 4 || random == 5 || random == 6 || random == 7 || random == 8)
+                    {
+                        tab[v1 + m][v2 + n] = 0;   
+                    }
+                    else
+                    {
+                        tab[v1 + m][v2 + n] = 1;
+                    }
+                }
+                else
+                {
+                    tab[v1 + m][v2 + n] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i < 50; i++)
+        {
+            for(int j = 0; j < 50; j++)
+            {
+                if(tab[i][j] == 0)
+                {
+                    if(tab[i+1][j] == 1 || tab[i][j + 1] == 1 || tab[i - 1][j] == 1 || tab[i][j - 1] == 1)
+                    {
+                        tab[i][j] = 3;
+                    }
+                }
+            }
+        }
     }
 }
 
-void color(int tab[20][20])
+void color(int tab[50][50])
 {
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    for(int i = 0; i < 20; i++)
+    for(int i = 0; i < 50; i++)
     {
-        for(int j = 0; j < 20; j++)
+        for(int j = 0; j < 50; j++)
         {
             switch(tab[i][j])
             {
                 case 0:
-                    SetConsoleTextAttribute(hOut, FOREGROUND_GREEN);
-                    cout << "0  ";
+                    SetConsoleTextAttribute(hOut, BACKGROUND_GREEN);
+                    cout << "   ";
                     break;
                 case 1:
-                    SetConsoleTextAttribute(hOut, FOREGROUND_BLUE);
-                    cout << "1  ";
+                    SetConsoleTextAttribute(hOut, BACKGROUND_BLUE);
+                    cout << "   ";
+                    break;
+                case 3:
+                    SetConsoleTextAttribute(hOut, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_INTENSITY);
+                    cout << "   ";
                     break;
                 default:
                     SetConsoleTextAttribute(hOut, FOREGROUND_RED);
@@ -127,13 +168,14 @@ void color(int tab[20][20])
                     break;
             }
         }
-        cout << endl;
+        SetConsoleTextAttribute(hOut, 7);
+        cout << "\n";
     }
 }
 
 int main()
 {
-    int world[20][20];
+    int world[50][50];
 
     gland(world);
     color(world);
